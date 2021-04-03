78a442d38eda30277fabecd323f534201c35a2a0
Christopher Booth <booth.christopherm@gmail.com>
2021-04-02 20:38:20 -0500

    (Feat) Add components Cart Deals and Home
    Adds three components: Cart, Deals, and Home.\n  These components will house all necessary subcomponents relating to their routes

---
+++ src/components/Deals/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Deals = () => {
+  return (
+    <div>
+      <p>Hello from Deals.  This is placeholder text</p>
+    </div>
+  )
+}
+
+export default Deals
---
+++ src/components/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <p>Hello from Home. This is placeholder text</p>
+    </div>
+  )
+}
+
+export default Home
